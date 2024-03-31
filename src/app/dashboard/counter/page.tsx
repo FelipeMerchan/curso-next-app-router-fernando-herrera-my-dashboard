85f56e535bb54ee7e8a01c8992a8fd974ff4258b
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-03-30 22:51:35 -0500

    43. Pensemos en hojas y pequeños componentes

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,33 +1,18 @@
-'use client'
-
-import { useState } from "react";
+import { CartCounter } from "@/shopping-cart";
 
 export const metadata = {
-  title: 'Counter page',
+  title: 'Shopping Cart',
   description: 'Contador del carrito de compras'
 }
 
 export default function CounterPage() {
-  const [counter, setCounter] = useState(0)
-
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span>Productos en el carrito</span>
-      <span className="text-9xl">{counter}</span>
-      <div className="flex">
-        <button
-          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-          onClick={() => setCounter(counter - 1)}
-        >
-          -1
-        </button>
-        <button
-          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-          onClick={() => setCounter(counter + 1)}
-        >
-          +1
-        </button>
-      </div>
+      {/* Podemos enviar datos del lado del servidor a client
+      components, por ejemplo, leer de una base de datos el valor
+      y pasarlo en la prop value: */}
+      <CartCounter value={20} />
     </div>
   );
 }
