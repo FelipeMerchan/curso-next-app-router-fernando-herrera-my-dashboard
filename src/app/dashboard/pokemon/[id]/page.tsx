7f3647843a4ec5920dc3e35db4f3c335ad5443e9
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-05-19 12:30:33 -0500

    57. Cargar información del Pokemon por ID

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,11 +1,25 @@
+import { Pokemon } from "@/pokemon";
+
 interface Props {
   params: { id: string };
 }
 
-export default function PokemonPage({ params }: Props) {
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: 'force-cache',
+  }).then(response => response.json());
+  console.log('Se cargó a', { pokemon: pokemon.name })
+
+  return pokemon;
+}
+
+export default async function PokemonPage({ params }: Props) {
+  const pokemon = await getPokemon(params.id);
+
   return (
     <div>
       <h1>Pokémon {params.id}</h1>
+      <div>{JSON.stringify(pokemon)}</div>
     </div>
   );
 }
--- src/pokemon/index.ts
+++ src/pokemon/index.ts
@@ -1,5 +1,6 @@
 export { PokemonCard } from './components/PokemonCard'
 export { PokemonGrid } from './components/PokemonGrid'
 
+export type { Pokemon } from './interfaces/pokemon'
 export type { PokemonResponse } from './interfaces/pokemon-response'
 export type { SimplePokemon } from './interfaces/simple-pokemon'
