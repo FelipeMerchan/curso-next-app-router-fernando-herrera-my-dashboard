18478f6dca88eda4be79fa025351c6b38be3af07
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-05-19 12:48:53 -0500

    58. Metadata dinámica

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,12 +1,31 @@
 import { Pokemon } from "@/pokemon";
+import { Metadata } from "next";
 
 interface Props {
   params: { id: string };
 }
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  /* Como estamos cachenado la respuesta de la petición, así
+  ejecutemos 2 veces en esta página el método getPokemon,
+  no vamos a tener problemas de performance porque la segunda petición
+  no se realizará y en su lugar se usará el cache: */
+  const { id, name } = await getPokemon(params.id);
+
+  return {
+    title: `#${id} - ${name}`,
+    description: `Página del Pokémon ${name}`,
+  }
+}
+
 const getPokemon = async (id: string): Promise<Pokemon> => {
   const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
     cache: 'force-cache',
+    /* next: {
+      Cada 6 meses se va a reconstruir - revalidar - esta pantalla
+      automáticamente:
+      revalidate: 60 * 60 * 30 * 6
+    } */
   }).then(response => response.json());
   console.log('Se cargó a', { pokemon: pokemon.name })
 
@@ -19,7 +38,7 @@ export default async function PokemonPage({ params }: Props) {
   return (
     <div>
       <h1>Pokémon {params.id}</h1>
-      <div>{JSON.stringify(pokemon)}</div>
+      <div>{pokemon.name}</div>
     </div>
   );
 }
