a71717a39588cd3a1ce1e15719cf7406a328ce5c
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-05-22 22:07:34 -0500

    82. Counter Reducer y acciones

--- src/store/counter/counterSlice.ts
+++ src/store/counter/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 interface CounterState {
   count: number;
@@ -14,9 +14,27 @@ const counterSlice = createSlice({
   /* Los reducers son las acciones que queremos llamar
   desde cualquier lugar de la aplicación para que cambien
   el valor del state:*/
-  reducers: {}
+  reducers: {
+    addOne(state) {
+      state.count++;
+    },
+    substractOne(state) {
+      if (state.count === 0) return;
+
+      state.count--;
+    },
+    resetCount(state, action:  PayloadAction<number>) {
+      /* No se recomienda cambiar el estado con valores que estén fuera
+      del scope de la acción, por ejemplo, hacer una petición http
+      acquí y guardar la respuesta en el state. Debemos únicamente usar el
+      payload que enviamos en el action. Se recomienda que estas
+      acciones de Redux sean funciones puras. */
+      if (action.payload < 0) action.payload = 0;
+      state.count = action.payload;
+    }
+  },
 });
 
-export const {} = counterSlice.actions;
+export const { addOne, substractOne, resetCount } = counterSlice.actions;
 
 export default counterSlice.reducer;
