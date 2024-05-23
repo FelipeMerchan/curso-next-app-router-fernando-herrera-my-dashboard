59976ab2fe7c56498e0f270c8449dc1da4c100e6
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-05-22 23:01:48 -0500

    84. Tarea - Store, Props y Links

---
+++ src/components/dashboard/WidgetsGrid.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useAppSelector } from '@/store'
+import { SimpleWidget } from './SimpleWidget'
+import { IoCartOutline } from 'react-icons/io5';
+
+export const WidgetsGrid = () => {
+  const inCart = useAppSelector(state => state.counter.count);
+
+  return (
+    <div className="flex flex-wrap p-2 items-center justify-center">
+      <SimpleWidget
+        icon={<IoCartOutline className='text-blue-600' size={70} />}
+        label='Contador'
+        subtitle='Productos agregados'
+        title={`${inCart}`}
+      />
+    </div>
+  )
+}
